3b4c5830a8a38bd59009c07975687efe1d112662
aiqinxuancai <aiqinxuancai@163.com>
2021-12-23 00:39:43 +0800

    update

---
+++ DotNetDllPathPatcherCMD/Program.cs
@@ -0,0 +1,32 @@
+﻿using DotNetDllPathPatcherWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                if (File.Exists(args[0]))
+                {
+                    string fileName = args[0];
+                    string binPath = "bin";
+                    string oldBinPath = "";
+                    Console.WriteLine("Start...");
+                    Conversion.ConversionDll(fileName, binPath, oldBinPath);
+                }
+            }
+
+
+            
+        }
+    }
+}
+
+
+
+
