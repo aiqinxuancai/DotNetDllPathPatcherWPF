bffc9875289db8687d1f366269b6e50ec51f9470
aiqinxuancai <aiqinxuancai@163.com>
2022-02-21 12:53:18 +0800

    测试裁剪dll的方案

--- DotNetDllPathPatcherCMD/Program.cs
+++ DotNetDllPathPatcherCMD/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DotNetDllPathPatcherCMD;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace DotNetDllPathPatcherCMD // Note: actual namespace depends on the project name.
@@ -21,6 +22,26 @@ namespace DotNetDllPathPatcherCMD // Note: actual namespace depends on the proje
                     {
                         Conversion.ConversionDll(fileName, binPath, oldBinPath);
                         Console.WriteLine("Success！");
+
+                        //完成
+                        //获取获取是否要运行某个EXE，运行后等待15秒，然后执行删除不引用的dll，然后结束进程
+
+                        string myLibraryShrink = args.First(a => a.StartsWith("--MyLibraryShrink:"));
+                        if (!string.IsNullOrWhiteSpace(myLibraryShrink))
+                        {
+                            var myLibraryShrinks = myLibraryShrink.Split(":");
+
+                            if (myLibraryShrinks.Length == 2)
+                            {
+                                var runFileName = myLibraryShrinks[1];
+                                RunLibraryShrink(fileName, binPath, runFileName);
+
+                            }
+
+                        }
+                        
+
+
                     }
                     catch (Exception ex)
                     {
@@ -41,6 +62,54 @@ namespace DotNetDllPathPatcherCMD // Note: actual namespace depends on the proje
 
 
             
+        }
+
+        private static void RunLibraryShrink(string fileName, string binPath, string runFileName)
+        {
+            if (File.Exists(runFileName))
+            {
+                Console.WriteLine($"Run {runFileName}");
+
+                var process = Process.Start(runFileName);
+
+                Console.WriteLine("Waiting...");
+
+                Thread.Sleep(15 * 1000);
+
+
+                //执行删除
+                var dllFiles = Directory.GetFiles($@"{Path.GetDirectoryName(fileName)}\{binPath}", "*.dll", SearchOption.TopDirectoryOnly);
+
+                foreach (var file in dllFiles)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                        Console.WriteLine($"Deleted {file}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+
+                }
+
+                process.Kill();
+                process.WaitForExit();
+
+                Thread.Sleep(3 * 1000);
+
+                //清除临时生成的文件？
+                var files = Directory.GetFiles(Path.GetDirectoryName(fileName), "*.*", SearchOption.TopDirectoryOnly);
+                foreach (var file in files)
+                {
+                    if (file != fileName)
+                    {
+                        File.Delete(file);
+                    }
+                }
+
+            }
         }
     }
 }
